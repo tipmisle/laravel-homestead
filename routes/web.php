a33d2423b82b4a92de6382b969fe96107203d6db
Josip Rajković <sirsir55@gmail.com>
2017-12-12 05:13:48 +0100

    Created routes for admin functions and one for accessing profile

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,31 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+//route root
+Route::get('/', 'HomeController@index');
 
-Route::get('/', function () {
-    return view('welcome');
+//login route
+Route::get('/login', 'HomeController@login');
+//group of routes for our adminController functions
+Route::group(
+    ['middleware' => ['admin']],
+    function(){
+
+        Route::get('/dashboard', 'AdminController@index');
+
+        Route::get('/calendar/create', 'AdminController@createCalendar');
+        Route::post('/calendar/create', 'AdminController@doCreateCalendar');
+
+        Route::get('/event/create', 'AdminController@createEvent');
+        Route::post('/event/create', 'AdminController@doCreateEvent');
+
+        Route::get('/calendar/sync', 'AdminController@syncCalendar');
+        Route::post('/calendar/sync', 'AdminController@doSyncCalendar');
+
+        Route::get('/events', 'AdminController@listEvents');
+
+        Route::get('/logout', 'AdminController@logout');
 });
+
+//profile route
+Route::get('/profile/{id}', 'UserController@profile');
