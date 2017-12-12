ee04c0c55360f86fbaebc01d1d4497d8000dc36e
Josip Rajković <sirsir55@gmail.com>
2017-12-12 05:07:20 +0100

    Modified migration for creating users table to suit our needs for API fields

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->text('token');
             $table->string('email')->unique();
-            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
