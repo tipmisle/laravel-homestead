996410472e6b966a364137cc692b0f6380e83549
Josip Rajković <sirsir55@gmail.com>
2017-12-12 05:19:13 +0100

    Created Home controller

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,70 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Googl;
+use App\User;
+use App\Calendar;
+use Illuminate\Http\Request;
+use Session;
+
+class HomeController extends Controller
+{
+    //homepage
+   public function index()
+   {
+        return view('login');
+   }
+
+   //login function
+   public function login(Googl $googl, User $user, Request $request)
+   {
+        //setting our client
+        $client = $googl->client();
+        //authenticating user
+        if ($request->has('code')) {
+            $client->authenticate($request->get('code'));
+            $token = $client->getAccessToken();
+
+            $plus = new \Google_Service_Plus($client);
+
+            $google_user = $plus->people->get('me');
+
+            $id = $google_user['id'];
+
+            $email = $google_user['emails'][0]['value'];
+            $first_name = $google_user['name']['givenName'];
+            $last_name = $google_user['name']['familyName'];
+
+            $has_user = $user->where('email', '=', $email)->first();
+            //checking if user exists in our database
+            if (!$has_user) {
+                //not yet registered
+                $user->email = $email;
+                $user->first_name = $first_name;
+                $user->last_name = $last_name;
+                $user->token = json_encode($token);
+                $user->save();
+                $user_id = $user->id;
+            } else {
+                $user_id = $has_user->id;
+            }
+            //create a session with our data
+            session([
+                'user' => [
+                    'id' => $user_id,
+                    'email' => $email,
+                    'first_name' => $first_name,
+                    'last_name' => $last_name,
+                    'token' => $token
+                ]
+            ]);
+
+            return redirect('/dashboard')
+                ->with('message', ['type' => 'success', 'text' => 'You are now logged in.']);
+
+        } else {
+            $auth_url = $client->createAuthUrl();
+            return redirect($auth_url);
+        }
+   }
+}
