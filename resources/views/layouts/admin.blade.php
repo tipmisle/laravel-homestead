50bb54928b6a7cad2cfc0257e01b34afe98b0e9d
Josip Rajković <sirsir55@gmail.com>
2017-12-12 05:30:31 +0100

    Modified admin view

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -15,6 +15,16 @@
         </header>
         @yield('content')
     </div>
+
+    @yield('attendee_template')
+
+    @yield('jquery_script')
+
+    @yield('handlebars_script')
+
+    @yield('jquery_datetimepicker_script')
+
+    @yield('create_event_script')
     <script
   src="https://code.jquery.com/jquery-3.2.1.js"
   integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE="
