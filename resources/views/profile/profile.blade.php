6f62e8b6af182bf4769d735ec97bccec7cfddbd9
Josip Rajković <sirsir55@gmail.com>
2017-12-14 04:30:49 +0100

    Added bootstrap modal to profile view

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -1,15 +1,56 @@
-@extends('layouts.admin')
+@extends('layouts.default')
 
 @section('content')
-<p>Currently signed in as:</p>
-<ul>
-    <li><a href="/calendar/create">Create Calendar</a></li>
-    <li><a href="/event/create">Create Event</a></li>
-    <li><a href="/calendar/sync">Sync Calendar</a></li>
-    <li><a href="/events">Events</a></li>
-    <li><a href="/logout">Logout</a></li>
-</ul>
+<h1>Koledar osebe: <u>{{ $user->first_name }} {{ $user->last_name }}</u></h1>
 <div id='calendar'></div>
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Create new event</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <form id="a">
+          <div class="modal-body">
+              <div class="form-group">
+                <label for="date">Date</label>
+                <p name="date" id="date"></p>
+              </div>
+              <div class="form-group">
+                <select class="form-control" name="calendar_id" id="calendar_id">
+                    @if(!empty($mycalendars))
+                        @foreach($mycalendars as $cal)
+                                <option value="{{ $cal->calendar_id }}">{{ $cal->title }}</option>
+                        @endforeach
+                    @endif
+                </select>
+              </div>
+              <div class="form-group">
+                <label for="time_s">Start time</label>
+                <input name="time_s" type="time" class="form-control" id="time_s" value="15:30">
+              </div>
+              <div class="form-group">
+                <label for="time_e">End time</label>
+                <input name="time_e" type="time" class="form-control" id="time_e" value="16:30">
+              </div>              
+              <div class="form-group">
+                <label for="summary">Summary</label>
+                <input name="summary" type="text" class="form-control" id="summary" placeholder="Summary">
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <p id="user_id" name="user_id" hidden>{{ $user->id }}</p>
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary">Save changes</button>
+          </div>
+        </form>
+    </div>
+  </div>
+</div>
+@stop
 
 @section('gcalscript')
 <script type="text/javascript">
