362245865a086850522e5aa389f0051126c65dc7
Josip Rajković <sirsir55@gmail.com>
2017-12-14 04:26:35 +0100

    Created modal submit for creating events

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -9,3 +9,53 @@ require('./bootstrap');
 require('jquery');
 require('moment');
 require('fullcalendar');
+require('axios');
+
+$("#f").on('submit', function(e) {
+	e.preventDefault();
+	var date = $("#date").text();
+	var summary = $("#summary").val();
+	var time_s = $("#time_s").val();
+	var time_e = $("#time_e").val();
+	var calendar_id = $("#calendar_id").val();
+	console.log(calendar_id);
+	axios.post('/event/create', {
+		date: date,
+		summary: summary,
+		time_s: time_s,
+		time_e: time_e,
+		calendarid: calendar_id
+	})
+		.then(function (response) {
+		$('#exampleModal').modal('hide');
+	})
+		.catch(function (error) {
+		console.log(error);
+	});
+});
+
+$("#a").on('submit', function(e) {
+	e.preventDefault();
+	var date = $("#date").text();
+	var summary = $("#summary").val();
+	var time_s = $("#time_s").val();
+	var time_e = $("#time_e").val();
+	var calendar_id = $("#calendar_id").val();
+	var user_id = $("#user_id").text();
+	var url = '/event/create/profile/' + user_id;
+	console.log(calendar_id);
+	axios.post(url, {
+		date: date,
+		summary: summary,
+		time_s: time_s,
+		time_e: time_e,
+		calendarid: calendar_id,
+		userid: user_id
+	})
+		.then(function (response) {
+		$('#exampleModal').modal('hide');
+	})
+		.catch(function (error) {
+		console.log(error);
+	});
+});
